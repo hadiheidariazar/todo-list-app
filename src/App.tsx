e5257b2f1a2016bc5300f5e2ff79b475c2762656
hadiheidariazar <hadiha010101@gmail.com>
2025-03-14 16:50:20 +0330

    coding todo list front at dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,21 @@ export default function App() {
   return (
     <div className="content w-full px-3">
       <div className="flex justify-center items-center h-screen">
-        <div className="bg-white w-full xs:w-[450px] h-max max-h-full sm:max-h-[80vh] my-3 sm:my-10 p-5 rounded-lg shadow-2xl shadow-gray-400">
+        <div className="bg-white dark:bg-zinc-700 w-full xs:w-[450px] h-max max-h-full sm:max-h-[80vh] my-3 sm:my-10 p-5 rounded-lg shadow-2xl shadow-gray-400 dark:shadow-zinc-900">
           <header className="flex justify-between items-center gap-x-2">
-            <h1 className="text-4xl font-medium text-sky-600">Todo List</h1>
-            <button className="text-2xl bg-sky-600 rounded-full p-1.5 flex justify-center items-center text-white" onClick={toggleTheme}>
+            <h1 className="text-4xl font-medium text-sky-600 dark:text-sky-500">Todo List</h1>
+            <button className="text-2xl bg-sky-600 rounded-full p-2 flex justify-center items-center text-white" onClick={toggleTheme}>
               <FiSun className="hidden dark:block" />
               <FiMoon className="dark:hidden" />
             </button>
           </header>
-          <form className="flex justify-between items-center gap-x-2 my-10 child:py-1.5 child:rounded-md child:border-[1px] child:border-sky-600">
-            <input type="text" placeholder="Enter your task..." className="px-2 w-full focus:appearance-none focus:outline-none placeholder:text-zinc-500" />
+          <form className="flex justify-between items-center gap-x-2 my-10 child:py-1.5 child:rounded-md child:border-[2px] child:border-sky-600">
+            <input type="text" placeholder="Enter your task..." className="px-2 w-full focus:appearance-none focus:outline-none placeholder:text-zinc-500 placeholder:dark:text-white/65 bg-inherit" />
             <button className="bg-sky-600 text-white px-4 w-max">Add</button>
           </form>
-          <div className="todo-wrapper divide-y divide-gray-300 h-max max-h-96 sm:max-h-80 overflow-y-auto rounded-md">
+          <div className="todo-wrapper divide-y divide-gray-300 dark:divide-white/20 h-max max-h-96 sm:max-h-80 overflow-y-auto rounded-md">
             {todos.length ? (
+              <>
               <div className="todo flex justify-between items-center gap-x-2.5 py-3 px-0.5">
                 <div className="overflow-x-auto overflow-y-hidden">
                   <p className="font-medium w-max">Learn NextJs</p>
@@ -37,6 +38,7 @@ export default function App() {
                   </button>
                 </div>
               </div>
+              </>
             ) : <p className="font-medium text-lg text-center">No todos yet :|</p>}
           </div>
         </div>
